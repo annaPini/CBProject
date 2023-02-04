fixed postprocessing; optimized density calculation; visual enhancements of output
- post-processing in step3 fixed, now the protein is correctly aligned and the water doesn't diffuse into the protein
- renamed WA1 and optimized density calculation using numpy, now it's possible to calculate for hundreds of frames
- WA1 now adds a large amount of water in the outside regions of the box, to later avoid plotting low-density artefacts that were present in the corners
- Colors were adjusted in WA0 and WA3 to have a better contrast in the final visualization
- Optimized calc_rgyr in radius_of_gyration.py, now it uses a list comprehension

src_analysis/radius_of_gyration.py
@@ -1,21 +1,19 @@
 from parameters import *
-import numpy as np
 import MDAnalysis as mda
 import matplotlib.pyplot as plt
 
 def calc_rgyr(gro_dir,traj_dir,out_dir):
     traj = mda.Universe(str(gro_dir),str(traj_dir))
-    ca_atoms = traj.select_atoms("protein and name CA")
+
     print("...>>> Calculating Radius of Gyration...")
-    Rgyr = []
-    for ts in traj.trajectory:
-        Rgyr.append(ca_atoms.radius_of_gyration())
+    Rgyr = [traj.select_atoms("protein and name CA").radius_of_gyration() for _ in traj.trajectory[:]]
+
     print("...>>> Saving RGYR...")
     np.save(out_dir,Rgyr,allow_pickle=False)
     print("done")
 
 def vis_rgyr(rgyr_dir):
-    rgyr=np.load(rgyr_dir)
+    rgyr = np.load(rgyr_dir)
     plt.plot(rgyr)
     plt.xlabel("frames")
     plt.ylabel("Radius of Gyration")
@@ -23,7 +21,6 @@ def vis_rgyr(rgyr_dir):
 
 
 if __name__ == "__main__":
-
     for run in RUNS:
         POSTDIR = get_POSTDIR(run)
         RGYR_OUT = POSTDIR / f"{TRAJECTORY_NAME}-rgyr.npy"
@@ -35,4 +32,3 @@ if __name__ == "__main__":
 
         plt.title(run)
         vis_rgyr(RGYR_OUT)
-    

src_analysis/rmsf.py
@@ -1,5 +1,5 @@
 from parameters import *
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import MDAnalysis as mda
 from MDAnalysis.analysis.rms import RMSF
 
@@ -8,7 +8,7 @@ def calc_rmsf(gro_dir, traj_dir, out_dir):
     ca_atoms = traj.select_atoms("protein and name CA")
     print("calculating RMSF...")
     rmsf_CA = RMSF(ca_atoms).run()
-    np.save(out_dir, rmsf_CA.rmsf, allow_pickle=False)
+    np.save(out_dir, rmsf_CA.rmsf, allow_pickle = False)
     print("done")
 
 def vis_rmsf(rmsf_dir):
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         RMSF_OUT = POSTDIR / f"{TRAJECTORY_NAME}-rmsf.npy"
         GRO = POSTDIR / f"{SYSTEM_NAME}.gro"
         XTC = POSTDIR / f"{TRAJECTORY_NAME}.xtc"
-        
+
         if not RMSF_OUT.exists():
             calc_rmsf(GRO, XTC, RMSF_OUT)
 